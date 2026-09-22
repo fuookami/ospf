2bd08e195865c66e6f4c9a3dd9eeeef6472b7a0f
fuookami <fuookami@users.noreply.github.com>
2026-09-22 13:06:14 +0000

    deploy: 4f140da800506db3436c063ca8858926950bd3e8

---
+++ assets/chunks/swimlanesDiagram-VR7AAH4N.Co5Nt6UR.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW.b0wuOfIm.js";import{_ as a}from"../app.MZkBBvNd.js";import"./chunk-5VM5RSS4.C_vzyxNw.js";import"./chunk-XXDRQBXY.Bl-07pDl.js";import"./chunk-POPQ4Y6H.BJWkmw2C.js";import"./chunk-F27PBJKO.CwYFYbgf.js";import"./channel.TMAz36yz.js";import"./framework.veCcaNqE.js";import"./theme.BJGqG8Dp.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
